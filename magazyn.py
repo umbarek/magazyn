aa953a2440fd0d715d7c05f8533a6763f1d8634f
Tomasz Pograniczny <tpograniczny@gmail.com>
2022-09-07 14:26:48 +0200

    Add menu

---
+++ magazyn.py
@@ -0,0 +1,10 @@
+print("\033[1m" + "Witaj w programie MAGAZYN\n" + "\033[0m")
+
+items = ("name", "quantity", "unit", "unit_price")
+
+while True:
+    input("Co mogę dla Ciebie zrobić?\n")
+    if exit:
+        print("Kończę działanie programu")
+        exit()
+        
